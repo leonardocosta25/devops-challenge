3c9407f050387a0fea3cb73a9ae9e1c8a2ad2d62
Leonardo Costa <leonardocosta25@hotmail.com>
2025-05-14 15:31:51 -0300

    Arrumando o playbook

--- generate_inventory.sh
+++ generate_inventory.sh
@@ -5,8 +5,6 @@ IP=$(terraform -chdir=terraform output -raw instance_ip)
 
 # Cria ou sobrescreve o inventory do Ansible
 cat <<EOF > ansiblev2/inventory.ini
-[all:vars]
-ansible_ssh_common_args: '-o UserKnownHostsFile=/dev/null'
 [servers]
 $IP ansible_user=ubuntu ansible_ssh_private_key_file=~/.ssh/aws-key.pem
 EOF
